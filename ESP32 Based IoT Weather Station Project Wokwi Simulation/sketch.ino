20f0fb6bb84d8445c14b61c976b0715dc690843b
Diegooz <85436549+Pixelssquad@users.noreply.github.com>
2024-08-26 01:35:34 +0200

    Update sketch.ino

--- ESP32 Based IoT Weather Station Project Wokwi Simulation/sketch.ino
+++ ESP32 Based IoT Weather Station Project Wokwi Simulation/sketch.ino
@@ -1,174 +1,111 @@
-//original file
-//https://github.com/Rafiq191/ESP32-Based-IoT-Weather-Station-Project-Wokwi-Simulation/blob/main/ESP32%20Based%20IoT%20Weather%20Station%20Project%20Wokwi%20Simulation/sketch.ino
-
+// Powered By Arduino classroom Youtube Channel
+// oRIGINAL https://github.com/filmcup/Esp32_youtube_counter_with_oled/blob/main/Youtube_counter/YoutubeApi.cpp
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <YoutubeApi.h>
 #include <ArduinoJson.h>
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#define OLED_RESET 16
 
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
-
-
-const char* ssid     = "OOZTEST";         // SSID, nome della tua rete wifi
-const char* password = "diegodiego";                    // Password on network
-String APIKEY = "5ec3304b3ee7e4b92acfaa668a3c0ca8";
-String CityID = "3179778";          //Codice della tua localita, https://openweathermap.org/
+#define SCREEN_WIDTH 128 // OLED width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED height, in pixels
 
 
-bool id = false;
-WiFiClient client;
-char servername[] = "api.openweathermap.org";  // remote server we will connect to
-String result;
+//Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+const char ssid[] = "OOZTEST";
+const char password[] = "diegodiego"; 
+
+#define API_KEY "QUI VA MESSA L'API KEY DI YOUTUBE" 
+#define CHANNEL_ID "UCm3gzmmPBf8AwbEvsANWBBg"
+
+WiFiClientSecure client;
+YoutubeApi api(API_KEY, client);
+
+unsigned long timeBetweenRequests = 60 * 1000;  // 60 seconds, in milliseconds
 
 void setup() {
+  Serial.begin(115200);
 
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c); //สั่งให้จอ OLED เริ่มทำงานที่ Address 0x3C
+  display.clearDisplay(); // ลบภาพในหน้าจอทั้งหมด
+  display.setTextSize(1); // กำหนดขนาดตัวอักษร
+  display.setTextColor(WHITE);
+  display.setCursor(0,0); // กำหนดตำแหน่ง x,y ที่จะแสดงผล
+  display.println(" FOLLOWER COUNTER ");
+  display.setCursor(0,20);
+  display.setTextSize(2);
+  display.setTextColor(BLACK, WHITE); //กำหนดข้อความสีขาว ฉากหลังสีดำ
+  display.println(" DIEGOOZ ");
+  display.setCursor(0,9);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("UN PROGETTO CON ESP32");
+  display.setCursor(0,48);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("YOUTUBE @PIXELSSQUAD"); // แสดงผลข้อความ www.Myarduino.net
+  display.display();
 
-   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
 
-  Serial.print("Connessione in corso.. ");
-  WiFi.mode(WIFI_STA);   //   create wifi station
+  Serial.print("\nConnecting to WiFi: ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
 
- 
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3D for 128x64
-  delay(200);
-  display.clearDisplay();
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);        // Draw white text
-  display.setCursor(0, 0);            // Start at top-left corner
-  display.print("connessione..");
-  display.display();
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
     Serial.print(".");
-    display.print(".");
-    display.display();
+    delay(500);
   }
-
-  Serial.println("");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  display.clearDisplay();
-  display.setCursor(0, 0);
-  display.println("Connected ");
-  display.println("IP Address: ");
-  display.println(WiFi.localIP());
-  display.display();
-  delay(1000);
-  display.clearDisplay();
-
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP address: ");
+  display.print("ISCRITTI: ");
+  display.println(api.channelStats.subscriberCount);
+  IPAddress ip = WiFi.localIP();
+  Serial.println(ip);
+  client.setInsecure();
 }
 
-void loop()                    
-{
-  if (client.connect(servername, 80))
-  { //starts client connection, checks for connection
-    client.println("GET /data/2.5/weather?id=" + CityID + "&units=metric&APPID=" + APIKEY);
-    client.println("Host: api.openweathermap.org");
-    client.println("User-Agent: ArduinoWiFi/1.1");
-    client.println("Connection: close");
-    client.println();
+void loop() {
+  if (api.getChannelStatistics(CHANNEL_ID)) {
+    Serial.println("\n-------Stats---------");
+
+    Serial.print("Subscriber Count: ");
+    Serial.println(api.channelStats.subscriberCount);
+
+    Serial.print("View Count: ");
+    Serial.println(api.channelStats.viewCount);
+
+    Serial.print("Video Count: ");
+    Serial.println(api.channelStats.videoCount);
+
+    Serial.println("------------------------");
+
+    display.clearDisplay(); // ลบภาพในหน้าจอทั้งหมด
+    display.setTextSize(1); // กำหนดขนาดตัวอักษร
+    display.setTextColor(WHITE);
+    display.setCursor(0,0); // กำหนดตำแหน่ง x,y ที่จะแสดงผล
+    
+    display.println("\n DIEGOOZ YT CHANNEL");
+   display.println("-=-=-=-=-=-=-=-=-=-=-");
+    display.print("ISCRITTI: ");
+    display.println(api.channelStats.subscriberCount);
+
+    display.print("Views: ");
+    display.println(api.channelStats.viewCount);
+
+    display.print("Video: ");
+    display.println(api.channelStats.videoCount);
+
+    display.println("---------------------");
+    display.println("ISCRIVITI AL CANALE");
+    display.display();
   }
-  else {
-    Serial.println("connection failed");        //error message if no client connect
-    Serial.println();
-  }
-
-  while (client.connected() && !client.available())
-    delay(1);                                          //waits for data
-  while (client.connected() || client.available())
-  { //connected or data available
-    char c = client.read();                     //gets byte from ethernet buffer
-    result = result + c;
-  }
-
-  client.stop();                                      //stop client
-  result.replace('[', ' ');
-  result.replace(']', ' ');
-  //Serial.println(result);
-  char jsonArray [result.length() + 1];
-  result.toCharArray(jsonArray, sizeof(jsonArray));
-  jsonArray[result.length() + 1] = '\0';
-  StaticJsonDocument<1024> doc;
-  DeserializationError  error = deserializeJson(doc, jsonArray);
-
-
-  if (error) {
-    Serial.print(F("deserializeJson() failed with code "));
-    Serial.println(error.c_str());
-    return;
-  }
-
-  String location = doc["name"];
-  String country = doc["sys"]["country"];
-  int temperature = doc["main"]["temp"];
-  int humidity = doc["main"]["humidity"];
-  float pressure = doc["main"]["pressure"];
-  int id = doc["id"];
-  float Speed = doc["wind"]["speed"];
-  int degree = doc["wind"]["deg"];
-  float longitude = doc["coord"]["lon"];
-  float latitude = doc["coord"]["lat"];
-
-  Serial.println();
-  Serial.print("Country: ");
-  Serial.println(country);
-  Serial.print("Location: ");
-  Serial.println(location);
-  Serial.print("Location ID: ");
-  Serial.println(id);
-  Serial.printf("Temperature: %d°C\r\n", temperature);
-  Serial.printf("Humidity: %d %%\r\n", humidity);
-  Serial.printf("Pressure: %.2f hpa\r\n", pressure);
-  Serial.printf("Wind speed: %.1f m/s\r\n", Speed);
-  Serial.printf("Wind degree: %d°\r\n", degree);
-  Serial.printf("Longitude: %.2f\r\n", longitude);
-  Serial.printf("Latitude: %.2f\r\n", latitude);
-
-  display.clearDisplay();
-  display.setCursor(0, 0);            // Start at top-left corner
-  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-  display.print(" Loc: ");
-  display.print(country);
-  display.print(" ");
-  display.println(location);
- 
-  display.println();
-  display.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
-  display.print("T: ");
-  display.print(temperature);
-  display.print((char)247);
-  display.print("C     ");
-  display.print("H: ");
-  display.print(humidity);
-  display.println("%  ");
-  display.print("Pressure:");
-  display.print(pressure);
-  display.println("hpa");
-  display.print("WS: ");
-  display.print(Speed);
-  display.print("m/s  ");
-  display.print("WA: ");
-  display.print(degree);
-  display.println((char)247);
-  display.print("Lat: ");
-  display.print(latitude);
-  display.print(" ");
-  display.print("Lon: ");
-  display.println(longitude);
-  display.setTextColor(SSD1306_BLACK, SSD1306_WHITE);
-  display.print(" SEGUIMI SU YOUTUBE");
-  display.print("          DIEGOOZ        ");
-  
-
-
-
-  display.display();
-  
-  delay(600000);     // 10 minuts delay
-
+  delay(timeBetweenRequests);
 }
